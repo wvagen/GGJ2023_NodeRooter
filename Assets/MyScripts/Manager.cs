29feb0d803dd58deac5e581742b8351ae4c6df08
Mouadh Mkadmi <36245246+wvagen@users.noreply.github.com>
2023-02-04 09:13:22 +0100

    Version 1.1

--- Assets/MyScripts/Manager.cs
+++ Assets/MyScripts/Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour
 {
@@ -10,15 +11,52 @@ public class Manager : MonoBehaviour
     Camera myCam;
     Node followedNode;
 
+    Dictionary<int,Node> spawnedNodes = new Dictionary<int, Node>();
+    public List<Node> duplicatedNodes = new List<Node>();
+
     private void Start()
     {
         myCam = Camera.main;
         followedNode = FindObjectOfType<Node>();
     }
 
+    public void Insert_Node(int nodeIndex, Node newNode)
+    {
+        duplicatedNodes.Add(newNode);
+        spawnedNodes[nodeIndex] = newNode;
+    }
+
+    public void Empty_Slot(int nodeIndex)
+    {
+        spawnedNodes.Remove(nodeIndex);
+
+    }
+
+    public bool Is_Node_Exists(int nodeIndex)
+    {
+        return spawnedNodes.ContainsKey(nodeIndex);
+    }
+
     private void Update()
     {
         Cam_Follow();
+        Restart_Scene_Trigger();
+        Log_Dictionnary();
+    }
+
+    void Log_Dictionnary()
+    {
+        foreach (KeyValuePair<int, Node> entry in spawnedNodes)
+        {
+            Debug.Log(entry.Key + " : " + entry.Value);
+            // do something with entry.Value or entry.Key
+        }
+        
+    }
+
+    void Restart_Scene_Trigger()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     void Cam_Follow()
@@ -29,8 +67,21 @@ public class Manager : MonoBehaviour
         myCam.transform.position = nodeFol;
     }
 
-    public void Set_Node_To_Follow(Node followedNode)
+    public void Set_Node_To_Follow(Node followedNode,int nodeIndex)
     {
         this.followedNode = followedNode;
+        followedNode.myIndex = nodeIndex;
+    }
+
+    public void Stop_All_Duplicated_Nodes(Node node)
+    {
+        for (int i = 0; i < duplicatedNodes.Count; i++)
+        {
+            if (duplicatedNodes[i].myIndex == node.myIndex)
+            {
+                duplicatedNodes[i].Stop_Node();
+            }
+        }
+        //duplicatedNodes.Remove(node);
     }
 }
--- Assets/MyScripts/Node.cs
+++ Assets/MyScripts/Node.cs
@@ -9,6 +9,8 @@ public class Node : MonoBehaviour
     public TrailRenderer myTrail;
     public Manager gameMan;
 
+    public int myIndex = 0;
+
     [SerializeField] private float lineThickness = 0.2f;
 
     Rigidbody2D myRig;
@@ -52,14 +54,14 @@ public class Node : MonoBehaviour
 
         GameObject tempNode = Instantiate(node, rightNode, Quaternion.identity);
         tempNode.GetComponent<Manager>();
-        gameMan.Set_Node_To_Follow(tempNode.GetComponent<Node>());
+        gameMan.Set_Node_To_Follow(tempNode.GetComponent<Node>(),0);
+        gameMan.Insert_Node((int)rightNode.x, this);
 
         myLine.startWidth = lineThickness;
         myLine.endWidth = lineThickness;
 
         myRig = tempNode.GetComponent<Rigidbody2D>();
         myRig.velocity = Vector2.down * gameMan.nodeSpeed;
-
         gameMan.isGameStarted = true;
     }
 
@@ -68,26 +70,33 @@ public class Node : MonoBehaviour
         Vector3 rightNode = transform.position + Vector3.right * margin;
         Vector3 leftNode = transform.position - Vector3.right * margin;
 
-        myRig.velocity = Vector2.zero;
+        Stop_Node();
+        gameMan.Stop_All_Duplicated_Nodes(this);
 
         //rightNode.y -= margin;
         //leftNode.y -= margin;
 
-        GameObject tempNode = Instantiate(node, rightNode, Quaternion.identity);
-        myLine.positionCount = 2;
+        GameObject tempNode = Instantiate(node, rightNode, Quaternion.identity); myLine.positionCount = 2;
         myLine.SetPosition(0, transform.position);
         myLine.SetPosition(1, tempNode.transform.position);
         tempNode.GetComponent<Manager>();
-        gameMan.Set_Node_To_Follow(tempNode.GetComponent<Node>());
+        gameMan.Set_Node_To_Follow(tempNode.GetComponent<Node>(),(int)rightNode.x);
+        gameMan.Insert_Node((int)rightNode.x, tempNode.GetComponent<Node>());
 
         GameObject tempNode2 = Instantiate(node, leftNode, Quaternion.identity);
         myLine.positionCount += 2;
         myLine.SetPosition(2, transform.position);
         myLine.SetPosition(3, tempNode2.transform.position);
         tempNode2.GetComponent<Manager>();
-
-
+        gameMan.Set_Node_To_Follow(tempNode2.GetComponent<Node>(), (int)leftNode.x);
+        gameMan.Insert_Node((int)leftNode.x, tempNode2.GetComponent<Node>());
     }
 
+    public void Stop_Node()
+    {
+        myRig.velocity = Vector2.zero;
+        isClicked = true;
+        gameMan.Empty_Slot((int)transform.position.x);
+    }
 
 }
